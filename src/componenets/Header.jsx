0b095fa6ed615365e54d5720bb6ba96ced42c571
saidugurlu <ugurlumsaid@gmx.de>
2022-04-21 11:17:52 +0200

    SOLVE 2 STATUS

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,4 +1,4 @@
-export const Header = ({ status }) => {
+/* export const Header = ({ status }) => {
   return (
     <>
       <h1>Employee Site</h1>
@@ -9,4 +9,28 @@ export const Header = ({ status }) => {
       )}
     </>
   );
-};
+}; */
+
+
+export const Header = ({ status }) => {
+
+	let message = '';
+	switch (status) {
+		case 'internalUser':
+			message = 'This is our team:';
+			break;
+		case 'externalUser':
+			message = 'The summer employee picnic is on May 15.';
+			break;
+		default:
+			message = '******* HAVE A NICE DAY ******';
+			break;
+	}
+
+	return (
+		<>
+			<h1>Employee Site</h1>
+			<div className="message">{message}</div>
+		</>
+	);
+}
