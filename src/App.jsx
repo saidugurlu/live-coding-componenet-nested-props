7635bc6973a0ff074be4e79d27941c3abcef2f82
saidugurlu <ugurlumsaid@gmx.de>
2022-04-21 10:54:54 +0200

    userIsExternal  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import './App.scss';
-import employees from './data/employees.json';
+import "./App.scss";
+import employees from "./data/employees.json";
+import { Header } from "./componenets/Header";
+import { Footer } from "./componenets/Footer";
+
+const userIsExternal = false;
 
 function App() {
-	return (
-		<div className="App">
-			<h1>Employee Site</h1>
-			<ul>
-				{employees.map((emp, index) => {
-					return <li>{emp.firstName} {emp.lastName}</li>;
-				})}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header  userIsExternal={userIsExternal}/>
+      <ul>
+        {employees.map((emp, index) => {
+          return (
+            <li>
+              {emp.firstName} {emp.lastName}
+            </li>
+          );
+        })}
+      </ul>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componenets/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = ({userIsExternal}) => {
+	return (
+		<>
+			<h1>Employee Site</h1>
+            {userIsExternal ?(
+                <div className="message">This is our team:</div>
+            ):(<div className="message">The summer employee picnic is on May 15.</div>
+            )}
+			
+			
+		</>
+	);
+};
